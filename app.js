b15e89b1eeae2bde05e3b295cf595ff5a3cad95c
shawnzey <shawnzey.syed@ontariotechu.net>
2025-11-06 22:58:42 -0500

    Initial commit - Express portfolio

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/* installed 3rd paty packages */
+let  createError = require('http-errors');
+let  express = require('express');
+let  path = require('path');
+let  cookieParser = require('cookie-parser');
+let  logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let  indexRouter = require('./routes/index');
+let  usersRouter = require('./routes/users');
 
-var app = express();
+let  app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,13 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get('/goodbyeworld', (req, res) => {
-  res.send('Goodbye World!');
-});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -38,7 +36,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {title:'Error'});
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET Home page */
+router.get(['/', '/home'], function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
-});
-
-/* GET About page. */
+/* GET About page */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About us' });
+  res.render('about', { title: 'About Me' });
 });
 
-/* GET products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+/* GET Projects page */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
 });
 
-/* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
-});
-
-/* GET home page. */
+/* GET Contact page */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact us' });
+  res.render('contact', { title: 'Contact Me' });
 });
+
 module.exports = router;
