b6ae7871a8961d69a10e8b710bbe812026dbd52a
shawnzey <shawnzey.syed@ontariotechu.net>
2025-11-06 23:13:55 -0500

    Added Express app files

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET Home page */
-router.get(['/', '/home'], function(req, res, next) {
+/* GET home page. */
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET About page */
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me' });
+  res.render('index', { title: 'About us' });
 });
 
-/* GET Projects page */
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
 });
 
-/* GET Contact page */
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET home page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
+  res.render('index', { title: 'Contact us' });
 });
-
 module.exports = router;
